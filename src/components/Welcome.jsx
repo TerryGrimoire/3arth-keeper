2be597b6d2cf1582b53ad8908ebbb1c64b85bb57
Terry Grimoire <terrt.grimoire@gmail.com>
2022-05-11 12:17:13 +0200

    image import

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import terre from "../assets/terre.gif";
 
 function Welcome() {
   return (
     <main className="home_main_tg">
-      <img src="./src/assets/terre.gif" className="home_logo_tg" alt="logo" />
+      <img src={terre} className="home_logo_tg" alt="logo" />
       <Link to="/Engagement">
         <button type="button" className="home_bouton_tg">
           AGIR <br /> MAINTENANT
